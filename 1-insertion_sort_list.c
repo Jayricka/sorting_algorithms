31ba7e526ff219772a2d2f1dadc302cbe09fac11
jayricka <jayricka17@gmail.com>
2023-07-26 03:07:51 +0300

    sorted algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,41 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
- * using the Insertion sort algorithm.
- *
- * @list: Pointer to the head of the doubly linked list
- */
+* insertion_sort_list - Sorts a doubly linked list of integers
+*                       in ascending order using insertion sort algorithm.
+*
+* @list: Double pointer to the head of the linked list.
+*/
 void insertion_sort_list(listint_t **list)
 {
-	if (!list || !(*list) || !((*list)->next))
-		return;
+listint_t *current, *insertion_point, *temp;
 
-	listint_t *current, *insertion_point, *temp;
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+return;
 
-	current = (*list)->next;
-	while (current)
-	{
-		insertion_point = current->prev;
-		temp = current;
-		current = current->next;
+current = (*list)->next;
 
-		while (insertion_point && insertion_point->n > temp->n)
-		{
-			if (insertion_point->prev)
-				insertion_point->prev->next = temp;
-			if (temp->next)
-				temp->next->prev = insertion_point;
-			insertion_point->next = temp->next;
-			temp->prev = insertion_point->prev;
-			temp->next = insertion_point;
-			insertion_point->prev = temp;
+while (current != NULL)
+{
+insertion_point = current;
+temp = current->prev;
 
-			if (!temp->prev)
-				*list = temp;
+while (temp != NULL && temp->n > insertion_point->n)
+{
+temp->next = insertion_point->next;
+if (insertion_point->next != NULL)
+insertion_point->next->prev = temp;
 
-			print_list(*list);
-			insertion_point = temp->prev;
-		}
-	}
+insertion_point->next = temp;
+insertion_point->prev = temp->prev;
+temp->prev = insertion_point;
+
+if (insertion_point->prev != NULL)
+insertion_point->prev->next = insertion_point;
+else
+*list = insertion_point;
+
+print_list(*list);
+temp = insertion_point->prev;
+}
+
+current = current->next;
+}
 }
